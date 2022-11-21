e0a7e7270f96af881d3e48bdef691c1d3e4c908e
Greg Howard <greg@howards.org>
2022-11-21 16:43:16 -0500

    Commented

--- src/p3.py
+++ src/p3.py
@@ -1,25 +1,20 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Nov 20 18:37:19 2022
+# Greg Howard & Srikanth Bonkuri
 
-@author: srika
-"""
+# Count the light objects in an image with a dark background
 
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sns
+import cv2 as cv # Import the OpenCV library
 
-#import cv
-import cv2 as cv
+# For now we are handling a hard coded local images.
+# We may consider improving to have this deal with a series of images.
+image = cv.imread("data/image_green.png")
 
-image = cv.imread("../data/image_green.png")
-
-r = len(image);
-c = len(image[0]);
+r = len(image)
+c = len(image[0])
 print(r,"x",c)
 print("Total Pixels:", r*c)
 
+# For now we are handling all pixels in one of 3 categories.
+# We may consider a more nuanced approach based on input from visual studies and cell biology experts.
 d = 0
 w = 0
 g = 0
@@ -52,6 +47,7 @@ print("Dark Pixels:", d)
 print("Bright Pixels:", w)
 print("Gray Pixels:", g)
 
+# imshow function is native to C++, so we may find a Python alternative for displaying a greyscale reduction image
 #imshow("abc", M);
 #imwrite("abc.png", M);
 #waitKey(0);
@@ -60,9 +56,11 @@ R = M
 k = 0
 t, m = 0, 0
 
+# BFS approach to counting cells, because it's easier to visualize
+# DFS approach is laid out here: https://www.geeksforgeeks.org/find-the-number-of-islands-using-dfs/
 q = []  #Queue
 cells = []
-white = 0
+white = 0 # We need to figure out how to get actual count of white pixels per cell
 
 for i in range(r):
     for j in range(c):
@@ -79,7 +77,7 @@ for i in range(r):
             result = q[0]
             x = result[0]
             y = result[1]
-            q.pop(0);
+            q.pop(0)
             #r[k][l]=0;
             k += 1
             for a in range(x-1, x+2):
@@ -90,20 +88,19 @@ for i in range(r):
                     if R[i,j,0] == 250:
                         white += 1
                     #r[a][b] = 0;
-                    R[a,b,0] = 0;
-                    R[a,b,1] = 0;
-                    R[a,b,2] = 0;
+                    R[a,b,0] = 0
+                    R[a,b,1] = 0
+                    R[a,b,2] = 0
                 
         if k>32:
             t +=1
-            cells.append([k, white])
+            cells.append([k, white]) # Not working yet
             #white = 0
-        if k > m: m = k;
+        if k > m: m = k
         #cout<<t<<"*";
         k = 0
         white = 0
     
-
 print("Total Chunks:", t)
 print("Largest Chunk:", m)
 print(cells)
